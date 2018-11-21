a53600b744155b89f7c55fb148cbc2a8bf585f4c
phlin <phlinhng@outlook.com>
2018-11-22 00:26:49 +0800

    fix some details

--- helpers/read_aqi.py
+++ helpers/read_aqi.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 
+# core operation
 def read_aqi(DIRNAME):
     print("Start importing files from", DIRNAME, sep=' ',end='\n')
     for root, dirs, files in os.walk(DIRNAME, topdown=False):
@@ -8,7 +9,20 @@ def read_aqi(DIRNAME):
         for name in files:
             input=pd.concat([input,pd.read_csv(os.path.join(root, name),encoding='big5')])
             print("Reading",os.path.join(root, name),sep= ' ',end='\n')
-        return input
+    print("Done importing files from", DIRNAME, sep=' ',end='\n')
+    return input
+
+# print all folder names in the current directory
+def get_dirnames():
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in dirs:
+            print(os.path.join(root, name))
+
+# print all filenames in the given folder
+def get_filenames(DIRNAME):
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
 
 
 """ Example:
