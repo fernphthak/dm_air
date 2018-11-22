117da4af26198352db2ffd0c2c882aac5628ae84
phlin <phlinhng@outlook.com>
2018-11-22 14:56:45 +0800

    add index resetting

--- helpers/read_aqi.py
+++ helpers/read_aqi.py
@@ -6,12 +6,12 @@ from datetime import datetime
 def read_aqi(DIRNAME):
     print("Start importing files from", DIRNAME, sep=' ',end='\n')
     for root, dirs, files in os.walk(DIRNAME, topdown=False):
-        input=pd.DataFrame()
+        output=pd.DataFrame()
         for name in files:
-            input=pd.concat([input,pd.read_csv(os.path.join(root, name),encoding='big5')])
+            output=pd.concat([output,pd.read_csv(os.path.join(root, name),encoding='big5')])
             print("Reading",os.path.join(root, name),sep= ' ',end='\n')
     print("Done importing files from", DIRNAME, sep=' ',end='\n')
-    return input
+    return output.reset_index()
 
 # print all folder names in the current directory
 def get_dirnames():
